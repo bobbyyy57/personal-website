2cc0bdd3a0c630351be0e8d3e1271123ecc64b47
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-07-11 01:39:27 -0700

    added arrow hover

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -7,9 +7,9 @@ const Intro = () => {
   return (
     <>
       <div className="flex justify-center items-center h-screen">
-        <div className="flex flex-col w-1/6 space-y-5 font-extralight pb-[10%] font-outfit text-sm">
+        <div className="flex flex-col w-1/6 pb-[10%]">
           <div className="flex justify-center">
-            <div className="flex w-2/12 items-center">
+            <div className="flex w-2/12 items-center mb-[10%]">
               <Image
                 src={Face}
                 alt="Landing"
@@ -18,13 +18,18 @@ const Intro = () => {
               />
             </div>
           </div>
-          <div>hey hey hey, welcome to my little spot on the internet. </div>
-          <div>my name is robert, but feel free to call me bobby :) </div>
-          <div>
-            i appreciate those who are bold, genuine, and unapologetic.{" "}
+          <div className="space-y-8 font-extralight font-outfit text-sm">
+            <div>hey hey hey, welcome to my little spot on the internet. </div>
+            <div>my name is robert, but feel free to call me bobby :) </div>
+            <div>
+              i appreciate those who are bold, genuine, and unapologetic.{" "}
+            </div>
+            <div>let’s get to know who i am.</div>
           </div>
-          <div>let’s get to know who i am.</div>
-          <HiOutlineArrowRight />
+
+          <button className="pt-[15%] duration-300 hover:-translate-y-1">
+            <HiOutlineArrowRight />
+          </button>
         </div>
       </div>
     </>
