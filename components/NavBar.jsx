0fe733806e37b5d7ef94fab1bafd9f1d1a02134c
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-07-11 23:26:28 -0700

    navbar finished

---
+++ components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="flex justify-center font-outfit font-light pt-[1%]">
+        <div className="flex justify-between w-8/12">
+          <div className="w-2/12 flex justify-start">bobbylerias</div>
+          <div className="flex justify-center space-x-[7%] w-full">
+            <div>projects</div>
+            <div>experiences</div>
+            <div>about me</div>
+          </div>
+          <div className="w-2/12 flex justify-end">contact me</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
---
+++ components/Projects.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+// import Image from "next/image";
+// import Link from "next/link";
+// import RParts from "../public/rparts.png";
+
+const Projects = () => {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center w-6/12">
+          <div className="w-full">
+            <div className="w-5/12">
+              <div className="font-montserrat font-medium text-bob-boldGray text-lg">
+                PROJECTS
+              </div>
+              <div className="font-outfit font-extralight text-bob-borderGray text-xs">
+                a few fun projects made fra few fun projects made fn projects
+                made era few fun projects made f
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Projects;
