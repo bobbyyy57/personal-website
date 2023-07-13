f10964613f0840f5dfd0eddb75573b5d73840d1f
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-07-13 14:54:14 -0700

    experiences done

--- components/Experiences.jsx
+++ components/Experiences.jsx
@@ -2,81 +2,123 @@ import React from "react";
 import Accordion from "react-bootstrap/Accordion";
 import { Col, Row } from "react-bootstrap";
 import { useState } from "react";
-import { FaPlus } from "react-icons/fa";
-import { FaMinus } from "react-icons/fa";
+import { PiCaretRightThin, PiCaretDownThin } from "react-icons/pi";
+// import { FaMinus } from "react-icons/fa";
+import Image from "next/image";
 
 const faqs = [
   {
-    q: "This is a question is it 1?",
-    a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius sit amet mattis vulputate enim nulla aliquet. Amet tellus cras adipiscing enim eu turpis egestas pretium.",
+    logo: "/ablogo.png",
+    company: "ANHEUSER-BUSCH",
+    date: "SUMMER 2022",
+    role: "TECHNOLOGY TRAINEE PROGRAM INTERN",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius sit amet mattis vulputate enim nulla aliquet. Amet tellus cras adipiscing enim eu turpis egestas pretium.",
+    tech: ["NEXT.JS", "TAILWINDCSS", "FIGMA", "MONGODB"],
   },
   {
-    q: "This is a question is it 2?",
-    a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius sit amet mattis vulputate enim nulla aliquet. Amet tellus cras adipiscing enim eu turpis egestas pretium.",
+    logo: "/ablogo.png",
+    company: "ANHEUSER-BUSCH",
+    date: "SUMMER 2022",
+    role: "TECHNOLOGY TRAINEE PROGRAM INTERN",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius sit amet mattis vulputate enim nulla aliquet. Amet tellus cras adipiscing enim eu turpis egestas pretium.",
+    tech: ["NEXT.JS", "TAILWINDCSS", "FIGMA", "MONGODB"],
   },
   {
-    q: "This is a question is it 3?",
-    a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius sit amet mattis vulputate enim nulla aliquet. Amet tellus cras adipiscing enim eu turpis egestas pretium.",
-  },
-  {
-    q: "This is a question is it 4?",
-    a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius sit amet mattis vulputate enim nulla aliquet. Amet tellus cras adipiscing enim eu turpis egestas pretium.",
-  },
-  {
-    q: "This is a question is it 5?",
-    a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius sit amet mattis vulputate enim nulla aliquet. Amet tellus cras adipiscing enim eu turpis egestas pretium.",
-  },
-  {
-    q: "This is a question is it 6?",
-    a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius sit amet mattis vulputate enim nulla aliquet. Amet tellus cras adipiscing enim eu turpis egestas pretium.",
-  },
-  {
-    q: "This is a question is it 7?",
-    a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius sit amet mattis vulputate enim nulla aliquet. Amet tellus cras adipiscing enim eu turpis egestas pretium.",
-  },
-  {
-    q: "This is a question is it 8?",
-    a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius sit amet mattis vulputate enim nulla aliquet. Amet tellus cras adipiscing enim eu turpis egestas pretium.",
+    logo: "/ablogo.png",
+    company: "ANHEUSER-BUSCH",
+    date: "SUMMER 2022",
+    role: "TECHNOLOGY TRAINEE PROGRAM INTERN",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididu sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Varius sit amet mattis vulputate enim nulla aliquet. Amet tellus cras adipiscing enim eu turpis egestas pretium.",
+    tech: ["NEXT.JS", "TAILWINDCSS", "FIGMA", "MONGODB"],
   },
 ];
 
-const Question = ({ index, question, answer }) => {
+const Question = ({ index, logo, company, role, date, tech, description }) => {
   const [toggle, setToggle] = useState(false);
 
-  const borderStyle = toggle
-    ? "border-x border-t border-orange-500 border-b-none"
-    : "border border-red-500";
-
-  const handleBodyClick = (e) => {
-    console.log("clicked");
-  };
-
   return (
-    <Col className="pb-2 !m-0 w-full">
+    <Col className="pb-2 !m-0 w-full !border-none font-outfit">
       <Accordion.Item
-        className="!border-none !p-0 underlayColor=transparent"
+        className="!border !rounded !p-0 underlayColor=transparent"
         eventKey={index}
       >
         <Accordion.Button
           onClick={() => setToggle(!toggle)}
-          className="!bg-bob-background text-black after:hidden p-0 focus:!shadow-none"
+          className="!bg-bob-background text-black after:hidden p-0 focus:!shadow-none  shadow-none"
         >
-          <div
-            className={`w-full flex justify-between items-center p-3 ${borderStyle}`}
-          >
-            <p className="text-black text-sm m-0">{question}</p>
-            <div className="text-xl m-2">
-              {toggle ? <FaMinus /> : <FaPlus />}
+          <div className="flex justify-between items-center p-3">
+            <div className="flex items-center">
+              <div className="w-2/12">
+                <Image
+                  src={logo}
+                  alt="Landing"
+                  layout="responsive"
+                  className="p-1"
+                  width="1"
+                  height="1"
+                />
+              </div>
+              <div className="w-10/12">
+                <div className="text-black  font-medium text-sm ml-5 ">
+                  {company}
+                </div>
+                <div className="text-black  font-extralight text-sm ml-5 ">
+                  {role}
+                </div>
+                <div className="text-black pt-1 font-montserrat font-thin text-xs ml-5 ">
+                  {date}
+                </div>
+              </div>
+            </div>
+
+            <div className="text-2xl text-black m-2">
+              {toggle ? <PiCaretDownThin /> : <PiCaretRightThin />}
             </div>
           </div>
         </Accordion.Button>
 
         <Accordion.Body
-          className="!bg-bob-background  flex justify-left items-center border-x border-b border-pink-600"
+          className="!bg-bob-background p-0 flex justify-left items-center rounded"
           eventKey={index}
-          onClick={handleBodyClick}
         >
-          <p className="text-black text-sm p-3">{answer}</p>
+          <div className="flex items-start px-3 pb-3 w-full">
+            <div className="flex flex-col justify-start">
+              <div className="flex">
+                <div className="w-2/12 invisible">
+                  <Image
+                    src={logo}
+                    alt="Landing"
+                    layout="responsive"
+                    className="p-1"
+                    width="1"
+                    height="1"
+                  />
+                </div>
+                <div className="flex flex-col w-10/12 ml-5 ">
+                  <div className="text-black text-sm font-extralight ">
+                    {description}
+                  </div>
+                  <Row className="mt-4">
+                    {tech.map((entry, index) => (
+                      <Col
+                        key={index}
+                        className="uppercase font-outfit text-xs font-light rounded-full mx-1 py-1 px-3 border !max-w-fit "
+                      >
+                        {entry}
+                      </Col>
+                    ))}
+                  </Row>
+                </div>
+              </div>
+            </div>
+
+            <div className="text-2xl m-2 invisible ">
+              {toggle ? <PiCaretDownThin /> : <PiCaretRightThin />}
+            </div>
+          </div>
         </Accordion.Body>
       </Accordion.Item>
     </Col>
@@ -85,12 +127,12 @@ const Question = ({ index, question, answer }) => {
 
 const FAQ = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center pb-[20%]">
       <div className="flex flex-col justify-center items-center w-6/12">
         <div className="w-full pb-[5%]">
           <div className="w-5/12">
             <div className="font-montserrat font-medium text-bob-boldGray text-lg">
-              PROJECTS
+              EXPERIENCES
             </div>
             <div className="font-outfit font-extralight text-bob-borderGray text-xs">
               a few fun projects made fra few fun projects made fn projects made
@@ -107,10 +149,14 @@ const FAQ = () => {
                   defaultActiveKey="0"
                   alwaysOpen
                 >
-                  {faqs.slice(0, faqs.length / 2).map((question, index) => (
+                  {faqs.map((question, index) => (
                     <Question
-                      question={question.q}
-                      answer={question.a}
+                      logo={question.logo}
+                      company={question.company}
+                      role={question.role}
+                      date={question.date}
+                      description={question.description}
+                      tech={question.tech}
                       index={index}
                       key={index}
                     />
