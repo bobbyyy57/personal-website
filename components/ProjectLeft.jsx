4d36fd0b9e8fe599a1575336f728f191255ec890
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-07-26 16:05:18 -0700

    removed floating buttons for projects and changed into buttons to improve clarity

--- components/ProjectLeft.jsx
+++ components/ProjectLeft.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { BsGithub } from "react-icons/bs";
+// import { BsGithub } from "react-icons/bs";
 import { FaFigma } from "react-icons/fa";
 import { PiRocketLaunch } from "react-icons/pi";
 import { TbBrandNextjs } from "react-icons/tb";
@@ -54,21 +54,25 @@ const ProjectLeft = ({
                   href={github}
                   className="no-underline text-black"
                 >
-                  <BsGithub className="duration-300 hover:-translate-y-1 text-black" />
-                  {/* <div className="duration-300 hover:-translate-y-1 hover:bg-black hover:text-white text-xs  hover:border-none border rounded-full font-outfit px-2 py-1">GITHUB</div> */}
+                  {/* <BsGithub className="duration-300 hover:-translate-y-1 text-black" /> */}
+                  <div className="duration-300 hover:-translate-y-1 hover:bg-black hover:text-white text-xs  hover:border-none border rounded-full font-outfit px-2 py-1">
+                    GITHUB
+                  </div>
                 </Link>
                 <Link
                   target="_blank"
                   href={figma}
                   className="no-underline text-black"
                 >
-                  <FaFigma className="duration-300 hover:-translate-y-1 text-black" />
+                  {/* <FaFigma className="duration-300 hover:-translate-y-1 text-black" /> */}
 
-                  {/* <div className="duration-300 hover:-translate-y-1 text-xs border hover:bg-black hover:text-white hover:border-none rounded-full font-outfit px-2 py-1">FIGMA</div> */}
+                  <div className="duration-300 hover:-translate-y-1 text-xs border hover:bg-black hover:text-white hover:border-none rounded-full font-outfit px-2 py-1">
+                    FIGMA
+                  </div>
                 </Link>
                 {dev !== "" && (
                   <Link target="_blank" href={dev}>
-                    <PiRocketLaunch className="duration-300 hover:-translate-y-1 text-black" />
+                    {/* <PiRocketLaunch className="duration-300 hover:-translate-y-1 text-black" /> */}
                   </Link>
                 )}
               </div>
@@ -106,36 +110,39 @@ const ProjectLeft = ({
             <div className="font-outfit font-base text-bob-borderGray text-xs">
               Roles: {role}
             </div>
-
-            <div className="flex justify-between text-xl pt-3 max-lg:pb-4  w-full">
-              <div className=" w-fit space-x-2 flex">
+            <div className="flex text-gray-500 items-center py-2 space-x-2">
+              {nextjs && <TbBrandNextjs />}
+              {fig && <FaFigma />}
+              {mongo && <SiMongodb />}
+            </div>
+            <div className="flex justify-center text-xl max-lg:pb-4  w-full">
+              <div className=" w-full  space-x-2 flex">
                 <Link
                   target="_blank"
                   href={github}
-                  className="no-underline text-black"
+                  className="no-underline text-black  w-full"
                 >
-                  <BsGithub className="duration-300 hover:-translate-y-1 text-black" />
-                  {/* <div className="duration-300 hover:-translate-y-1 hover:bg-black hover:text-white text-xs  hover:border-none border rounded-full font-outfit px-2 py-1">GITHUB</div> */}
+                  {/* <BsGithub className="duration-300 hover:-translate-y-1 text-black" /> */}
+                  <div className="duration-300 text-center hover:-translate-y-1 hover:bg-black hover:text-white text-xs  hover:border-none border rounded-full font-outfit px-2 py-1">
+                    GITHUB
+                  </div>
                 </Link>
                 <Link
                   target="_blank"
                   href={figma}
-                  className="no-underline text-black"
+                  className="no-underline text-black w-full"
                 >
-                  <FaFigma className="duration-300 hover:-translate-y-1 text-black" />
+                  {/* <FaFigma className="duration-300 hover:-translate-y-1 text-black" /> */}
 
-                  {/* <div className="duration-300 hover:-translate-y-1 text-xs border hover:bg-black hover:text-white hover:border-none rounded-full font-outfit px-2 py-1">FIGMA</div> */}
+                  <div className="text-center duration-300 hover:-translate-y-1 text-xs border hover:bg-black hover:text-white hover:border-none rounded-full font-outfit px-2 py-1">
+                    FIGMA
+                  </div>
                 </Link>
                 {dev !== "" && (
                   <Link target="_blank" href={dev}>
                     <PiRocketLaunch className="duration-300 hover:-translate-y-1 text-black" />
                   </Link>
                 )}
-              </div>
-              <div className="flex text-gray-500 items-center space-x-2">
-                {nextjs && <TbBrandNextjs />}
-                {fig && <FaFigma />}
-                {mongo && <SiMongodb />}
               </div>
             </div>
           </div>
