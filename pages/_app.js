83ecaa6c4ce83fd50d0d3b8333af14cf6422e3df
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-08-17 00:06:09 -0700

    added hotjar tracking

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Outfit, Montserrat } from "next/font/google";
 
+import Script from "next/script";
+
 const outfit = Outfit({
   subsets: ["latin"],
   variable: "--font-outfit",
@@ -20,6 +22,20 @@ export default function App({ Component, pageProps }) {
       className={`${outfit.variable} ${montserrat.variable} bg-bob-background h-full`}
     >
       <Component {...pageProps} />
+      <Script
+        id="HotJar"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:3616493,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+        }}
+      ></Script>
     </main>
   );
 }
