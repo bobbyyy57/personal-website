a96f15a0b3b984133533d099bf79d6abd67ced7c
Robert Lerias <72523111+bobbyyy57@users.noreply.github.com>
2023-09-30 17:31:36 -0700

    removed dev link for kabo

--- components/Experiences.jsx
+++ components/Experiences.jsx
@@ -14,7 +14,7 @@ const info = [
     description:
       "Managed 2+ freelance projects from client communication and project estimation to delivery, maintaining a high level of client satisfaction while boosting business’ online presence by 50% \n\n Created wireframes, prototypes, and mockups using Figma to visualize design concepts and workflow \n\n Collaborated with clients to understand their specific requirements, translating them into functional website designs and features using Next.js and TailwindCSS",
     tech: ["NEXT.JS", "TAILWINDCSS", "FIGMA"],
-    attachment: "https://kabostudio.vercel.app",
+    attachment: "",
     attachment2: "",
   },
   {
